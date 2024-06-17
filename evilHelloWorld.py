c2ff374abdd6fd4e72410c52b872da0991112153
paguileram <p.aguileram@duocuc.cl>
2024-06-17 19:51:54 -0400

    testing

--- evilHelloWorld.py
+++ evilHelloWorld.py
@@ -1,2 +1,10 @@
+
 print("Evil Hello World be like ")
 print("Bye underWorld")
+
+with open("si.txt",'w') as sopas:
+    sopas.write("SOPAAAAAAAAAAAAAAAAAAAAAS")
+    
+with open("si.txt",'r') as sopa:
+    lector = sopa.read()
+    print(lector)
